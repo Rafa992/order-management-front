56ac81d721ca23bd272798021f1d12e6751b201c
Rafael <rafa92muhitdinov@gmail.com>
2024-12-13 03:12:01 +0500

    update

--- src/services/auth-token.service.ts
+++ src/services/auth-token.service.ts
@@ -17,10 +17,12 @@ export const saveTokenStorage = (accessToken: string, refreshToken: string) => {
   Cookies.set(EnumTokens.ACCESS_TOKEN, accessToken, {
     domain: domain,
     sameSite: "strict",
+    secure: process.env.NODE_ENV === "production",
   });
   Cookies.set(EnumTokens.REFRESH_TOKEN, refreshToken, {
     domain: domain,
     sameSite: "strict",
+    secure: process.env.NODE_ENV === "production",
   });
 };
 
