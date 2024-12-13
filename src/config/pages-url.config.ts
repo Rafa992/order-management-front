0d2c86d4ba8ed7af45634002afbae99471b92016
Rafael <rafa92muhitdinov@gmail.com>
2024-12-13 20:43:51 +0500

    update

--- src/config/pages-url.config.ts
+++ src/config/pages-url.config.ts
@@ -1,5 +1,5 @@
 class DASHBOARD {
-    HOME = '/'
+    HOME = process.env.NODE_ENV === "production" ? 'https://order-management-indol.vercel.app/' : '/'
     LOGIN = '/auth/login'
     REGISTER = '/auth/register'
 }
