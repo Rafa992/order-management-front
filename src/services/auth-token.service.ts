3fbc6dd71b950099e4eb38d10779a147d03e022c
Rafael <rafa92muhitdinov@gmail.com>
2024-12-13 20:31:52 +0500

    update

--- src/services/auth-token.service.ts
+++ src/services/auth-token.service.ts
@@ -4,6 +4,7 @@ export enum EnumTokens {
   "ACCESS_TOKEN" = "accessToken",
   "REFRESH_TOKEN" = "refreshToken",
 }
+const domain = process.env.NODE_ENV === "production" ? "order-management-indol.vercel.app" : "localhost";
 
 export const getAccessToken = () => {
   const accessToken = Cookies.get(EnumTokens.ACCESS_TOKEN);
@@ -12,23 +13,29 @@ export const getAccessToken = () => {
 
 export const saveTokenStorage = (accessToken: string, refreshToken: string) => {
 
-  const domain = process.env.NODE_ENV === "production" ? "order-management-indol.vercel.app" : "localhost";
 
   Cookies.set(EnumTokens.ACCESS_TOKEN, accessToken, {
-    // domain: domain,
-    // sameSite: "none",
-    // secure: process.env.NODE_ENV === "production",
+    domain: domain,
+    sameSite: "none",
+    secure: process.env.NODE_ENV === "production",
   });
   Cookies.set(EnumTokens.REFRESH_TOKEN, refreshToken, {
-    // domain: domain,
-    // sameSite: "none",
-    // secure: process.env.NODE_ENV === "production",
+    domain: domain,
+    sameSite: "none",
+    secure: process.env.NODE_ENV === "production",
   });
 };
 
 export const removeFromStorage = () => {
-  console.log('сработал removeFromStorage');
 
-  Cookies.remove(EnumTokens.ACCESS_TOKEN);
-  Cookies.remove(EnumTokens.REFRESH_TOKEN);
+  Cookies.remove(EnumTokens.ACCESS_TOKEN, {
+    domain: domain,
+    sameSite: "none",
+    secure: process.env.NODE_ENV === "production",
+  });
+  Cookies.remove(EnumTokens.REFRESH_TOKEN, {
+    domain: domain,
+    sameSite: "none",
+    secure: process.env.NODE_ENV === "production",
+  });
 };
